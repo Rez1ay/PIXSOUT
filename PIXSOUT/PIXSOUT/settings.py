4a1e779250acb91426dff6cda8dce07e276c78ca
Rez1ay <aleksandr-titov-2002@mail.ru>
2024-10-01 17:45:05 +0300

    Create gitignore; change SECRET_KEY and hide SECRET_KEY in .env(python-decouple); add to gitignore: venv, .env

--- PIXSOUT/PIXSOUT/settings.py
+++ PIXSOUT/PIXSOUT/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$5k6sefx&mk2f51jm2!kq=u%3-)7$p$z@-+v5h!&u$x^60md_v'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
