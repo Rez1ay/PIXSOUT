8e20d1a77bfa0d72f92630d46cfaec5a83ab1d27
Rez1ay <aleksandr-titov-2002@mail.ru>
2024-10-14 19:09:23 +0300

    Add for-cycle to forum.html

--- PIXSOUT/forum/views.py
+++ PIXSOUT/forum/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 
 
 def forum(request):
-    return render(request, 'forum/forum.html')
+    forum_themes = [
+        {'title': 'Новости', 'user': 'kaziyar', 'message': 'Мы релизнулись!', 'time': '6 дней назад'},
+        {'title': 'Техподдержка', 'user': 'duboooon', 'message': 'у меня игра крашается хелп', 'time': '2 дня назад'}
+    ]
+    data = {'forum_themes': forum_themes}
+
+    return render(request, 'forum/forum.html', context=data)
