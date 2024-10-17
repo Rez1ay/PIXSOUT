9ff3954f0a637dcd7b9545244ee2a5134241f432
Rez1ay <aleksandr-titov-2002@mail.ru>
2024-10-17 15:32:36 +0300

    Add models Theme, Publications in forum; add migration file

--- PIXSOUT/forum/models.py
+++ PIXSOUT/forum/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Theme(models.Model):
+    name = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+
+    objects = models.Manager()
+
+
+class Publication(models.Model):
+    author = models.CharField(max_length=255)
+    text = models.TextField()
+    time = models.DateTimeField(auto_now_add=True)
+    theme = models.ForeignKey(Theme, on_delete=models.CASCADE)
--- PIXSOUT/forum/views.py
+++ PIXSOUT/forum/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render
+from .models import Theme, Publication
 
 
 def forum(request):
-    forum_themes = [
-        {'title': 'Новости', 'user': 'kaziyar', 'message': 'Мы релизнулись!', 'time': '6 дней назад'},
-        {'title': 'Техподдержка', 'user': 'duboooon', 'message': 'у меня игра крашается хелп', 'time': '2 дня назад'}
-    ]
+    forum_themes = Theme.objects.all()
     data = {'forum_themes': forum_themes}
 
     return render(request, 'forum/forum.html', context=data)
