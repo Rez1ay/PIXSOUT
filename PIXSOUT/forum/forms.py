e45ed61650e60fbf4e412344f63ac6a4ee40c571
Rez1ay <aleksandr-titov-2002@mail.ru>
2024-10-29 09:51:42 +0300

    Update frontend for Publication form

--- PIXSOUT/forum/forms.py
+++ PIXSOUT/forum/forms.py
@@ -7,3 +7,6 @@ class AddPublicationForm(forms.ModelForm):
     class Meta:
         model = Publication
         fields = ['author', 'text']
+        widgets = {
+            'text': forms.Textarea(attrs={'rows': 5})
+        }
