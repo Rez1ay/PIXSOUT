adcf04159e70e064490fbbd83e4996afd3444843
Rez1ay <aleksandr-titov-2002@mail.ru>
2024-11-02 17:31:46 +0300

    Replace login function with a LoginView

--- PIXSOUT/users/forms.py
+++ PIXSOUT/users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
+from django.contrib.auth.forms import AuthenticationForm
 
 
-class LoginForm(forms.Form):
+class LoginForm(AuthenticationForm):
     username = forms.CharField(label='Логин', widget=forms.TextInput)
     password = forms.CharField(label='Пароль', widget=forms.PasswordInput)
--- PIXSOUT/users/views.py
+++ PIXSOUT/users/views.py
@@ -1,25 +1,16 @@
+from django.contrib.auth.views import LoginView
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
 from django.urls import reverse
 from .forms import LoginForm
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import logout
 
 
-def login_user(request):
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            form_data = form.cleaned_data
-            user = authenticate(request, username=form_data['username'], password=form_data['password'])
-            if user and user.is_active:
-                login(request, user)
-                return HttpResponseRedirect(reverse('home'))
-    else:
-        form = LoginForm()
-    data = {
-        'form': form
-    }
-    return render(request, 'users/login.html', context=data)
+class LoginUser(LoginView):
+    form_class = LoginForm
+    template_name = 'users/login.html'
+
+    def get_success_url(self):
+        return reverse('home')
 
 
 def logout_user(request):
