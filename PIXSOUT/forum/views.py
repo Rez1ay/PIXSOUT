6bef62b58f2a9a2aab30bdf8a2eeddea3798fa37
Rez1ay <aleksandr-titov-2002@mail.ru>
2024-11-02 09:34:39 +0300

    Update forum themes list - show the latest post

--- PIXSOUT/forum/views.py
+++ PIXSOUT/forum/views.py
@@ -4,7 +4,11 @@ from .forms import AddPublicationForm
 
 
 def forum(request):
-    forum_themes = Theme.objects.all()
+    forum_themes = []
+    for t in Theme.objects.all():
+        last_publication = Publication.objects.filter(theme=t).last()
+        last_publication.text = last_publication.text[:120] + '...'
+        forum_themes.append((t, last_publication))
     data = {'forum_themes': forum_themes}
 
     return render(request, 'forum/forum.html', context=data)
@@ -19,6 +23,7 @@ def theme(request, theme_name):
         if form.is_valid():
             new_publication = form.save(commit=False)
             new_publication.theme = current_theme
+            new_publication.author = request.user
             new_publication.save()
 
     form = AddPublicationForm()
